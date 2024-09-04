8cec53af6da2897582f32782b765fee2294c0dad
Arnold Melet <arnoldmelet18@gmail.com>
2024-09-05 01:44:29 +0200

    example app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AppAr from "./views/AppAr";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
+      <AppAr />
+      <AppAr />
+      <AppAr />
+      <AppAr />
+      <AppAr />
+      <AppAr />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/views/AppAr.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppAr() {
+  return <div>AppAr</div>;
+}
+
+export default AppAr;
