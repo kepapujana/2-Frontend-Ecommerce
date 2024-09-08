39884d5bd5bff12e5049b164c75a550b096dd5ef
Arnold Melet <arnoldmelet18@gmail.com>
2024-09-08 22:21:54 +0200

    ejemplo de homeview con productos randoms

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,52 @@
 import React from "react";
+import "./Home.styles.scss";
 
 const Home = () => {
   return (
     <>
-      <h1>This is the Home View</h1>
+      <h1>Mi Tienda Online</h1>
+
+      <section className="products">
+        <div className="product">
+          <img src="../img/hotwheels.jpg" alt="Producto 1" />
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1</p>
+          <p>Precio: $10.00</p>
+          <button>Comprar</button>
+        </div>
+
+        <div className="product">
+          <img src="../img/hotwheels.jpg" alt="Producto 1" />
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1</p>
+          <p>Precio: $10.00</p>
+          <button>Comprar</button>
+        </div>
+
+        <div className="product">
+          <img src="../img/hotwheels.jpg" alt="Producto 1" />
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1</p>
+          <p>Precio: $10.00</p>
+          <button>Comprar</button>
+        </div>
+
+        <div className="product">
+          <img src="../img/hotwheels.jpg" alt="Producto 1" />
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1</p>
+          <p>Precio: $10.00</p>
+          <button>Comprar</button>
+        </div>
+
+        <div className="product">
+          <img src="../img/hotwheels.jpg" alt="Producto 1" />
+          <h2>Producto 1</h2>
+          <p>Descripción del producto 1</p>
+          <p>Precio: $10.00</p>
+          <button>Comprar</button>
+        </div>
+      </section>
     </>
   );
 };
